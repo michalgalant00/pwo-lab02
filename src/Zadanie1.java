88b7b6f61bd1b104a822d0b06dd676a0671be7dc
michalgalant00 <s95401@pollub.edu.pl>
2023-10-11 11:01:05 +0200

    Zadanie 1 - wersja 1 + poprawka

--- src/Zadanie1.java
+++ src/Zadanie1.java
@@ -15,7 +15,7 @@
 
  BufferedWriter writer
  = new BufferedWriter(new FileWriter(fileName));
- for(int i=0; i<100; i++)
+ for(int i=0; i<=100; i++)
  writer.write(Fibonacci(i).toString() + "\n");
  writer.close();
 
